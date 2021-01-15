b1aee4dcd198a3a68b3a8cd6c069f339c8c7a0d7
Konstantin <konstantin1020@gmail.com>
2021-01-14 16:18:43 -0800

    added variables and if statements

--- script.js
+++ script.js
@@ -11,9 +11,31 @@ function writePassword() {
 
 function generatePassword() {
   var pwlength = parseInt(prompt('How many chars?'));
-  console.log(typeof pwlength);
-  console.log();
+  if (pwlength >= 8 && pwlength <= 128 && !isNaN(pwlength)) {
+    console.log ("valid entry")
+    var ifLowercase = confirm ("Would you like to include lowercase letters?") 
+    var ifUppercase = confirm ("Would you like to include uppercase letters?") 
+    var ifNumbers = confirm ("Would you like to include numbers?") 
+    var ifSpecialCharacters = confirm ("Would you like to include special characters?") 
+    console.log (ifLowercase, ifUppercase, ifNumbers, ifSpecialCharacters)
+  }
+
+  if (ifLowercase === false && ifUppercase === false && ifNumbers === false && ifSpecialCharacters === false) {
+    alert ("Password must include lowercase letters, uppercase letters, numbers, or special characters")
+    return generatePassword ()
+  }
+  return pwlength
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+// validate pwlength so that it is > 8 and < 128
+// ask the user if they want lowercase characters
+//  ... upercase
+// ... numbers
+// ... special characters 
+// validate that all of these are not false
+// get random characters from a pool of characters
+// return the password 
