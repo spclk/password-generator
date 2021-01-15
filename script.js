91738f5386099c170f5771368992b488a89709be
Konstantin <konstantin1020@gmail.com>
2021-01-14 22:57:26 -0800

    final updates

--- script.js
+++ script.js
@@ -10,32 +10,35 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var pwlength = parseInt(prompt('How many chars?'));
+  var pwlength = parseInt(prompt('How many characters?'));
   if (pwlength >= 8 && pwlength <= 128 && !isNaN(pwlength)) {
     console.log ("valid entry")
     var ifLowercase = confirm ("Would you like to include lowercase letters?") 
     var ifUppercase = confirm ("Would you like to include uppercase letters?") 
     var ifNumbers = confirm ("Would you like to include numbers?") 
     var ifSpecialCharacters = confirm ("Would you like to include special characters?") 
+    
     console.log (ifLowercase, ifUppercase, ifNumbers, ifSpecialCharacters)
+
   }
+
+    var allLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var allUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var allNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var allSpecialCharacters = ["~","!","#","$","%","^","&","*","(",")","_","-","+","=","`","<",",",">",".","?","/","{","}","[","]","|"];
 
   if (ifLowercase === false && ifUppercase === false && ifNumbers === false && ifSpecialCharacters === false) {
     alert ("Password must include lowercase letters, uppercase letters, numbers, or special characters")
     return generatePassword ()
   }
   return pwlength
+
+  
+  
+
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-
-// validate pwlength so that it is > 8 and < 128
-// ask the user if they want lowercase characters
-//  ... upercase
-// ... numbers
-// ... special characters 
-// validate that all of these are not false
-// get random characters from a pool of characters
-// return the password 
